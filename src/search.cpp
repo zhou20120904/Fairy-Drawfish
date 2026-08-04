7c535e7e96958d33ab898c87396a763c44651bdb
Zhou Jinyi <125183026+zhou20120904@users.noreply.github.com>
2026-08-04 22:53:49 +0800

    Refactor transposition table save logic

--- src/search.cpp
+++ src/search.cpp
@@ -1497,10 +1497,15 @@ moves_loop: // When in check, search starts from here
 
     // Write gathered information in transposition table
     if (!excludedMove && !(rootNode && thisThread->pvIdx))
+    {
+        Bound b = bestValue >= beta ? BOUND_LOWER :
+                  PvNode && bestMove ? BOUND_EXACT : BOUND_UPPER;
+        if (b == BOUND_EXACT) 
+            b = BOUND_NONE;
+
         tte->save(posKey, value_to_tt(bestValue, ss->ply), ss->ttPv,
-                  bestValue >= beta ? BOUND_LOWER :
-                  PvNode && bestMove ? BOUND_EXACT : BOUND_UPPER,
-                  depth, bestMove, ss->staticEval);
+                  b, depth, bestMove, ss->staticEval);
+    }
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
 
